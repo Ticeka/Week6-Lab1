71504b858ccd91000c0a74c2b69882a6b2ce985a
Thanakrit Kaewsomsri <tice132547@gmauil.com>
2024-12-03 04:11:36 +0700

    first commit

---
+++ WebPushLineNotify/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPushLineNotify.Models;
+
+namespace WebPushLineNotify.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LineNotify()
+        {
+            var model = new NotifyViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LineNotify(NotifyViewModel model)
+        {
+            var client = new RestClient("https://notify-api.line.me/api/notify");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Cache-Control", "no-cache");
+            request.AddHeader("Authorization", "Bearer 1f9Gp1HVtoJlB0zNg4bUIOiwcxP2oAnDpkiO5lzvnDE");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("undefined", "message=" + model.Message, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+
+            return View(model);
+        }
+    }
+}
